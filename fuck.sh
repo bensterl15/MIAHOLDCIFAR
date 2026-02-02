8773ea68f8f8fe80353615e5b2ac9ebdd7141eca
bensterl15 <bensterl15@gmail.com>
2026-02-02 00:37:03 -0500

    Removing profane script name

--- fuck.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ./data/train.lmdb ./data/validation.lmdb
-python util/convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=data/celeba/celeba-tfr --lmdb_path=./data --split=train
-python util/convert_tfrecord_to_lmdb.py --dataset=celeba --tfr_path=data/celeba/celeba-tfr --lmdb_path=./data --split=validation
